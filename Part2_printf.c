9ba7262554ba83eb331904958a75e08273cc32d3
KhadijahIdzham <k.taherah@gmail.com>
2020-01-14 19:17:39 +1000

    Part 1 - Print variables using printf

---
+++ Part2_printf.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+	int x = 10;
+
+	int y = x / 2;
+
+ 	//The magic number is - a string
+	//("") - a format string.  
+	//%i - format or conversion characters. goes into the format strings. We need to put a coversion for the numbers into a form  that computer understand. 
+	//\n - new line character
+	
+	printf ("The value of x is %i\nThe magic number is %i\n", x, y);
+	return 0;
+
+}
+
+
