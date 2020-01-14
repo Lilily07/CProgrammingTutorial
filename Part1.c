9d6bd43be7033cd002d47737a4b9d94174b2cd7d
KhadijahIdzham <k.taherah@gmail.com>
2020-01-14 18:26:47 +1000

    Part 1 - Statement, Variable, Declaration, Initialization

---
+++ Part1.c
@@ -0,0 +1,25 @@
+//Topics - Variable, Declaration, Initialization, Expression, Statement
+
+//SYNTAX - Format of  language - where do you put semicolon, equal signs, and others. rules.  
+
+
+
+#include <stdio.h>
+
+int main()
+{
+	int x;  //x is a variable. We declare the variable as int -integer(whole number only)
+	x = 10; //initialization the variable. We give value to the variable
+		//or can be written in one line as (int x = 10)
+
+	//Expression - anything that can be evaluated (to 1 value). x/2 is an expression which can be evaluated to 5
+	
+	int y = x / 2; //We declare and initialize the variable
+
+	//question - why not put directly (y = 10 /2), no need x
+	//when you are using variable x multiple times, we dont want to have to repeat over and over. Data x can change over time  
+	
+
+	printf();
+	return 0;
+}
